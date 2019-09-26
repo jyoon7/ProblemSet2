b85d55e6b9239e55dad88e91c74b967670d8487d
jyoon7 <jyoon7@berklee.edu>
2019-09-26 10:55:20 -0400

    Uploaded problem sets
    2.4 was completed before uploading.

---
+++ ProblemSet2.4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) 
+{
+    int i = 2019;
+    float f = 0.23f;
+    char c = 'm';
+    printf("%c, %0.2f, %i\n", c, f, i); 
+}
---
+++ ProblemSet2.5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    for(int stacks = 1; stacks > 8; stacks++)
+    {
+        printf("%i",stacks);
+    }
+}
